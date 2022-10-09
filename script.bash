d699bee198af69f205772655ce15a057c9ca39dd
artshellorok <artshellorok@gmail.com>
2022-10-09 15:49:17 +0300

    will work?

--- script.bash
+++ script.bash
@@ -2,115 +2,85 @@
 
 export LANG=en_US.UTF-8
 
-dc=0
-fc=0
+traverse(){
+    local path=$1
+    local prefix=$2
+    local files=( $( ls $path ) )
+    local n=${#files[@]}
+    local i
 
-tree ()
-{
-	dir=$1
-	parent=$2
-	subdirs=( $(ls $dir) )
-	
-	dc=0
-	fc=0
-	
-	i=0
-	for name in ${subdirs[@]}
-	do
-		(( i++ ))
-		child="\u251c\u2500\u2500\u0020"
-	
-		last=0
-		if [[ $i -eq ${#subdirs[@]} ]]
-		then
-			child="\u2514\u2500\u2500\u0020"
-			if [[ $3 -eq 1 ]] || [[ $4 -eq 0 ]]
-			then
-				last=1
-			fi
-		fi
-		
-		if [ -d "$dir/$name" ] && [[ $i -ne ${#subdirs[@]} ]]
-		then
-			nextparent="$parent\u2502\u00A0\u00A0\u0020"
-		else 
-			nextparent="$parent\u0020\u0020\u0020\u0020"
-		fi
-		
-		if [[ $last -eq 1 ]]
-		then
-			parent=""
-			for i in $(seq 1 $4)
-			do
-				parent="$parent\u0020\u0020\u0020\u0020"
-			done
-			nextparent="$parent\u0020\u0020\u0020\u0020"
-		fi
-		
-		file=0
-		for char in $(grep -o . <<< "$name")
-		do
-			if [ "$char" = "." ]
-			then
-				file=1
-			fi
-		done
-		
-		printf "$parent$child$name\n"
-		
-		if [ $file -eq 0 ] && [ -d "$dir/$name" ]
-		then
-			(( dc++ ))
-			subdirschild=( $(ls $dir/$name) )
-			if [ ${#subdirschild[@]} -ne 0 ]
-			then
-				printf "$(tree "$dir/$name" $nextparent $last $(( $4 + 1 )))\n"
-			fi
-		else
-			(( fc++ ))
-		fi
-	done
-	printf "+$dc $fc\n"
+
+    for (( i=0; i<n; i++ ))
+    do
+        local el=${files[$i]}
+
+        # if [[ $( basename $el ) -eq "\*" ]]; then
+        #     continue
+        # fi
+        if [[ $i -eq $(( n-1 )) ]]; then
+            local local_prefix="\u0020\u0020\u0020\u0020"
+            local pointer="\u2514\u2500\u2500\u0020"
+        else
+            local local_prefix="\u2502\u00A0\u00A0\u0020"
+            local pointer="\u251c\u2500\u2500\u0020"
+        fi
+        # echo $prefix $pointer $el
+        printf "${prefix}${pointer}"
+        # if [[ -L $el ]]; then
+        #     if [[ -d "$el" ]]; then
+        #         count_dir=$(( count_dir + 1 ))
+        #     else
+        #         count_files=$(( count_files + 1 ))
+        #     fi
+        #     echo "${el##*/} -> $( realpath $el )"
+        #     continue
+        # fi
+
+        if [[ -d "$path/$el" ]]; then
+            count_dir=$(( count_dir +1 ))
+            if ! [[ -r "$path/$el" ]]; then
+                echo "$el  [error opening dir]"
+            else
+                echo "$el"
+            fi
+            traverse "$path/$el" "$prefix$local_prefix"
+        else
+            echo "${el##*/}"
+            count_files=$(( count_files +1 ))
+        fi
+
+    done
+
 }
 
-direct=$1
-if [ "$direct" = "" ]
-then
-	direct="."
+count_dir=0
+count_files=0
+
+if [[ $# -eq 0 ]]; then
+    traverse '.' ''
 fi
 
-
-printf "$direct\n"
-
-res=$(tree $direct "" 0 0)
-IFS=$'\n' y=($res)
-
-for line in ${y[@]}
+for path in "$@"
 do
-	if [ "${line:0:1}" = "+" ]
-	then
-		nums=${line#*_}
-		IFS=$'\u0020' arr=($nums)
-		dc=$(( $dc + ${arr[0]} ))
-		fc=$(( $fc + ${arr[1]} ))
-	else
-		printf "$line\n"
-	fi
+    if ! [[ -d "$path" ]]; then
+        echo "$path  [error opening dir]"
+    else
+        count_dir=0
+        count_files=0
+        echo $path
+        traverse "$path" ""
+    fi
 done
-
 
 printf "\n"
 
-if [ $dc -eq 1 ]
-then
-	printf "1 directory, "
+if [[ $count_dir -eq 1 ]]; then
+    printf "1 directory, "
 else
-	printf "$dc directories, "
+    printf "$count_dir directories, "
 fi
-
-if [ $fc -eq 1 ]
-then
-	printf "1 file\n"
+if [[ $count_files -eq 1 ]]; then
+    printf "1 file\n"
 else
-	printf "$fc files\n"
+    printf "$count_files files\n"
 fi
