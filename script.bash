706d67593f28b1fdf92b4f9f2c43caa50b6f4a7a
artshellorok <artshellorok@gmail.com>
2022-10-09 15:54:50 +0300

    bl

--- script.bash
+++ script.bash
@@ -1,67 +1,118 @@
+
+
 #!/bin/bash
 
 export LANG=en_US.UTF-8
-shopt -s nullglob
-shopt -u expand_aliases
 
-traverse(){
-    local path=$1
-    local prefix=$2
-    local files=("$path"/*)
-    local n=${#files[@]}
-    local i
+dc=0
+fc=0
 
-
-    for (( i=0; i<n; i++ ))
-    do
-        local el=${files[$i]}
-
-        if [[ $i -eq $(( n-1 )) ]]; then
-            local local_prefix="\u0020\u0020\u0020\u0020"
-            local pointer="\u2514\u2500\u2500\u0020"
-        else
-            local local_prefix="\u2502\u00A0\u00A0\u0020"
-            local pointer="\u251c\u2500\u2500\u0020"
-        fi
-        printf "${prefix}${pointer}"
-
-        if [[ -d $el ]]; then
-            count_dir=$(( count_dir +1 ))
-            echo "${el##*/}"
-            traverse "$el" "$prefix$local_prefix"
-        else
-            echo "${el##*/}"
-            count_files=$(( count_files +1 ))
-        fi
-
-    done
-
+tree ()
+{
+	dir=$1
+	parent=$2
+	subdirs=( $(ls $dir) )
+	
+	dc=0
+	fc=0
+	
+	i=0
+	for name in ${subdirs[@]}
+	do
+		(( i++ ))
+		child="\u251c\u2500\u2500\u0020"
+	
+		last=0
+		if [[ $i -eq ${#subdirs[@]} ]]
+		then
+			child="\u2514\u2500\u2500\u0020"
+			if [[ $3 -eq 1 ]] || [[ $4 -eq 0 ]]
+			then
+				last=1
+			fi
+		fi
+		
+		if [ -d "$dir/$name" ] && [[ $i -ne ${#subdirs[@]} ]]
+		then
+			nextparent="$parent\u2502\u00A0\u00A0\u0020"
+		else 
+			nextparent="$parent\u0020\u0020\u0020\u0020"
+		fi
+		
+		if [[ $last -eq 1 ]]
+		then
+			parent=""
+			for i in $(seq 1 $4)
+			do
+				parent="$parent\u0020\u0020\u0020\u0020"
+			done
+			nextparent="$parent\u0020\u0020\u0020\u0020"
+		fi
+		
+		file=0
+		for char in $(grep -o . <<< "$name")
+		do
+			if [ "$char" = "." ]
+			then
+				file=1
+			fi
+		done
+		
+		printf "$parent$child$name\n"
+		
+		if [ $file -eq 0 ] && [ -d "$dir/$name" ]
+		then
+			(( dc++ ))
+			subdirschild=( $(ls $dir/$name) )
+			if [ ${#subdirschild[@]} -ne 0 ]
+			then
+				printf "$(tree "$dir/$name" $nextparent $last $(( $4 + 1 )))\n"
+			fi
+		else
+			(( fc++ ))
+		fi
+	done
+	printf "+$dc $fc\n"
 }
 
-count_dir=0
-count_files=0
-
-if [[ $# -eq 0 ]]; then
-    traverse '.' ''
-else
-    count_dir=0
-    count_files=0
-    path=$1
-    echo $path
-    traverse "$path" ""
+direct=$1
+if [ "$direct" = "" ]
+then
+	direct="."
 fi
+
+
+printf "$direct\n"
+
+res=$(tree $direct "" 0 0)
+IFS=$'\n' y=($res)
+
+for line in ${y[@]}
+do
+	if [ "${line:0:1}" = "+" ]
+	then
+		nums=${line#*_}
+		IFS=$'\u0020' arr=($nums)
+		dc=$(( $dc + ${arr[0]} ))
+		fc=$(( $fc + ${arr[1]} ))
+	else
+		printf "$line\n"
+	fi
+done
+
 
 printf "\n"
 
-if [[ $count_dir -eq 1 ]]; then
-    printf "1 directory, "
+if [ $dc -eq 1 ]
+then
+	printf "1 directory, "
 else
-    printf "$count_dir directories, "
+	printf "$dc directories, "
 fi
-if [[ $count_files -eq 1 ]]; then
-    printf "1 file\n"
+
+if [ $fc -eq 1 ]
+then
+	printf "1 file\n"
 else
-    printf "$count_files files\n"
+	printf "$fc files\n"
 fi
-# printf "\n$count_dir directories, $count_files files\n"
-shopt -u nullglob
