be9cd7a807890daad80d8c34c0246f6d9597fb7a
artshellorok <artshellorok@gmail.com>
2022-10-09 09:15:19 +0300

    shopt

--- script.bash
+++ script.bash
@@ -1,5 +1,5 @@
 export LANG=en_US.UTF-8
-
+shopt -s nullglob
 printEntity() {
     filepath=$1
     ind=$2
@@ -9,13 +9,13 @@ printEntity() {
     else
         printf "\u251c"
     fi
-    printf "\u2500\u2500\u0020$( basename ${filepath} )\n"
+    printf "\u2500\u2500\u0020${filepath##*/}\n"
 }
 
 traverse(){
     local path=$1
     local prefix=$2
-    local files=($path/*)
+    local files=("$path"/*)
     local n=${#files[@]}
     local i
 
@@ -23,7 +23,10 @@ traverse(){
     for (( i=0; i<n; i++ ))
     do
         local el=${files[$i]}
-
+        
+        # if [[ $( basename $el ) -eq "\*" ]]; then
+        #     continue
+        # fi
         if [[ $i -eq $(( n-1 )) ]]; then
             local local_prefix="\u0020\u0020\u0020\u0020"
         else 
@@ -35,7 +38,7 @@ traverse(){
 
         if [[ -d $el ]]; then
             count_dir=$(( count_dir +1 ))
-            traverse $el "$prefix$local_prefix"
+            traverse "$el" "$prefix$local_prefix"
         else
             count_files=$(( count_files +1 ))
         fi
@@ -55,8 +58,9 @@ do
         count_dir=0
         count_files=0
         echo $path
-        traverse $path ""
+        traverse "$path" ""
     fi
 done
 
 printf "\n$count_dir directories, $count_files files\n"
+shopt -u nullglob
