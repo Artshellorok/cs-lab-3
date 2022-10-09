d9c01697c78dfece653ac13426710486d8d8cbf2
artshellorok <artshellorok@gmail.com>
2022-10-09 08:51:02 +0300

    rewrite

--- script.bash
+++ script.bash
@@ -1,74 +1,62 @@
 export LANG=en_US.UTF-8
 
-printLevel() {
-    local i 
-    local j
-    for (( j=0; j<$2; j++ ))
-    do
-        printf "\u0020\u0020\u0020\u0020"
-    done 
-    for (( i=0; i<$(( $1 - $2 )); i++ ))
-    do
-        printf "\u2502\u00A0\u00A0\u0020"
-    done
-}
 printEntity() {
-    if [[ $2 -eq $(( $3 -1 )) ]]; then
-       printf "\u2514\u2500\u2500\u0020$( basename ${1} )\n"
+    filepath=$1
+    ind=$2
+    n=$3
+    if [[ $ind -eq $(( n -1 )) ]]; then
+       printf "\u2514"
     else
-        printf "\u251c\u2500\u2500\u0020$( basename ${1} )\n" 
+        printf "\u251c"
     fi
+    printf "\u2500\u2500\u0020$( basename ${filepath} )\n"
 }
-printDir() {
-    local list
-    local len
-    local i 
-    # list=( $( ls -1 $1 | tr '\n' ' ' ) )
-    # list=($1/*)
-    # list=($1/* "adas")
-    list=($1/*)
-    len=${#list[@]}
 
-    for (( i=0; i < $len; i++ ))
+traverse(){
+    local path=$1
+    local prefix=$2
+    local files=($path/*)
+    local n=${#files[@]}
+    local i
+
+
+    for (( i=0; i<n; i++ ))
     do
-        if [[ -d "${list[$i]}" ]]; then
-            count_dir=$((count_dir+1))
-            printLevel $2 $3
-            printEntity ${list[$i]} $i $len
-            # cd ${list[$i]}
-            count_sp=$3
-            if [[ $i -eq $(( $len -1 )) ]]; then
-                count_sp=$(( count_sp + 1 ))
-            fi
-            printDir "${list[$i]}" $(( $2 +1 )) $count_sp
-        else
-            count_files=$((count_files+1))
-            printLevel $2 $3
-            printEntity ${list[$i]} $i $len
+        local el=${files[$i]}
+
+        if [[ $i -eq $(( n-1 )) ]]; then
+            local local_prefix="\u0020\u0020\u0020\u0020"
+        else 
+            local local_prefix="\u2502\u00A0\u00A0\u0020"
         fi
+
+        printf "$prefix"
+        printEntity $el $i $n
+
+        if [[ -d $el ]]; then
+            count_dir=$(( count_dir +1 ))
+            traverse $el "$prefix$local_prefix"
+        else
+            count_files=$(( count_files +1 ))
+        fi
+
     done
-    # if [[ $2 -ne 0 ]]; then
-    #     cd ..
-    # fi
+
 }
 
 count_dir=0
 count_files=0
-count_arg=0
+
 for path in "$@"
 do
-    count_arg=$(( count_arg + 1 ))
     if ! [[ -d "$path" ]]; then
         echo "$path  [error opening dir]"
     else
-        echo "$path"
-        if [[ $count_arg -eq $# ]]; then
-            printDir $path 0 0
-        else
-            printDir $path 0 0
-        fi
+        count_dir=0
+        count_files=0
+        echo $path
+        traverse $path ""
     fi
 done
-
 
 printf "\n$count_dir directories, $count_files files\n"
