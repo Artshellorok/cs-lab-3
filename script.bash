80cfc1716b27988efbabc2e694c95cf1b21f09ce
artshellorok <artshellorok@gmail.com>
2022-10-09 19:02:05 +0300

    test

--- script.bash
+++ script.bash
@@ -3,6 +3,17 @@
 export LANG=en_US.UTF-8
 shopt -s nullglob
 shopt -u expand_aliases
+printEntity() {
+    filepath=$1
+    ind=$2
+    n=$3
+    if [[ $ind -eq $(( n -1 )) ]]; then
+       printf "\u2514"
+    else
+        printf "\u251c"
+    fi
+    printf "\u2500\u2500\u0020${filepath##*/}\n"
+}
 
 traverse(){
     local path=$1
@@ -15,31 +26,29 @@ traverse(){
     for (( i=0; i<n; i++ ))
     do
         local el=${files[$i]}
-
+        
+        # if [[ $( basename $el ) -eq "\*" ]]; then
+        #     continue
+        # fi
         if [[ $i -eq $(( n-1 )) ]]; then
             local local_prefix="\u0020\u0020\u0020\u0020"
             local pointer="\u2514\u2500\u2500\u0020"
-        else
+        else 
             local local_prefix="\u2502\u00A0\u00A0\u0020"
             local pointer="\u251c\u2500\u2500\u0020"
         fi
+        # echo $prefix $pointer $el
         printf "${prefix}${pointer}"
-
-		file=0
-		for char in $(grep -o . <<< "${el##*/}")
-		do
-			if [ "$char" = "." ]
-			then
-				file=1
-			fi
-		done
-
-        if [[ $file -eq 0 ]] && [[ -d $el ]]; then
-            count_dir=$(( count_dir +1 ))
-            echo "${el##*/}"
-            traverse "$el" "$prefix$local_prefix"
+        if [[ -L $el ]]; then
+            echo "${el##*/} -> $( realpath $el )"
         else
             echo "${el##*/}"
+        fi
+
+        if [[ -d $el ]]; then
+            count_dir=$(( count_dir +1 ))
+            traverse "$el" "$prefix$local_prefix"
+        else
             count_files=$(( count_files +1 ))
         fi
 
@@ -52,20 +61,23 @@ count_files=0
 
 if [[ $# -eq 0 ]]; then
     traverse '.' ''
-else
-    count_dir=0
-    count_files=0
-    path=$1
-    echo $path
-    traverse "$path" ""
 fi
 
-printf "\n"
-
+for path in "$@"
+do
+    if ! [[ -d "$path" ]]; then
+        echo "$path  [error opening dir]"
+    else
+        count_dir=0
+        count_files=0
+        echo $path
+        traverse "$path" ""
+    fi
+done
 if [[ $count_dir -eq 1 ]]; then
-    printf "1 directory, "
+    printf "\n1 directory, "
 else
-    printf "$count_dir directories, "
+    printf "\n$count_dir directories, "
 fi
 if [[ $count_files -eq 1 ]]; then
     printf "1 file\n"
