8fe08eaa2fe43cc603085dddab016f11ff136778
artshellorok <artshellorok@gmail.com>
2022-10-10 13:27:45 +0300

    test

--- script.bash
+++ script.bash
@@ -1,21 +1,11 @@
-export LANG=en_US.UTF-8
+#!/bin/bash
 
-printEntity() {
-    filepath=$1
-    ind=$2
-    n=$3
-    if [[ $ind -eq $(( n -1 )) ]]; then
-       printf "\u2514"
-    else
-        printf "\u251c"
-    fi
-    printf "\u2500\u2500\u0020$( basename ${filepath} )\n"
-}
+export LANG=en_US.UTF-8
 
 traverse(){
     local path=$1
     local prefix=$2
-    local files=($path/*)
+    local files=( $( ls $path ) )
     local n=${#files[@]}
     local i
 
@@ -23,20 +13,27 @@ traverse(){
     for (( i=0; i<n; i++ ))
     do
         local el=${files[$i]}
-
+        
         if [[ $i -eq $(( n-1 )) ]]; then
             local local_prefix="\u0020\u0020\u0020\u0020"
+            local pointer="\u2514\u2500\u2500\u0020"
         else 
             local local_prefix="\u2502\u00A0\u00A0\u0020"
+            local pointer="\u251c\u2500\u2500\u0020"
         fi
 
-        printf "$prefix"
-        printEntity $el $i $n
+        printf "${prefix}${pointer}"
 
-        if [[ -d $el ]]; then
+        if [[ -d "$path/$el" ]]; then
             count_dir=$(( count_dir +1 ))
-            traverse $el "$prefix$local_prefix"
+            if ! [[ -r "$path/$el" ]]; then
+                echo "$el  [error opening dir]"
+            else 
+                echo "$el"
+            fi
+            traverse "$path/$el" "$prefix$local_prefix"
         else
+            echo "${el##*/}"
             count_files=$(( count_files +1 ))
         fi
 
@@ -47,6 +44,10 @@ traverse(){
 count_dir=0
 count_files=0
 
+if [[ $# -eq 0 ]]; then
+    traverse '.' ''
+fi
+
 for path in "$@"
 do
     if ! [[ -d "$path" ]]; then
@@ -55,18 +56,19 @@ do
         count_dir=0
         count_files=0
         echo $path
-        traverse $path ""
+        traverse "$path" ""
     fi
 done
 
 printf "\n"
+
 if [[ $count_dir -eq 1 ]]; then
     printf "1 directory, "
 else
     printf "$count_dir directories, "
 fi
 if [[ $count_files -eq 1 ]]; then
-    printf "1 file"
+    printf "1 file\n"
 else
-    printf "$count_files files"
+    printf "$count_files files\n"
 fi
