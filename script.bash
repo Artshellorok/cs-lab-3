c483e9f637fc9a9cdbb6c0cd99c044fc363f4424
artshellorok <artshellorok@gmail.com>
2022-10-12 21:40:32 +0300

    remove garbage

--- script.bash
+++ script.bash
@@ -26,11 +26,7 @@ traverse(){
 
         if [[ -d "$path/$el" ]]; then
             count_dir=$(( count_dir +1 ))
-            if ! [[ -r "$path/$el" ]]; then
-                echo "$el  [error opening dir]"
-            else 
-                echo "$el"
-            fi
+            echo "$el"
             traverse "$path/$el" "$prefix$local_prefix"
         else
             echo "${el##*/}"
